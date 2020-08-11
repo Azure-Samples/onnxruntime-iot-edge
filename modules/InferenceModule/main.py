d613f56da35cc3f541cbc18535cb1d7fc163f6c1
Wolfgang Pauli <wopauli@microsoft.com>
2020-08-11 11:47:53 -0700

    blob conn str via env

--- modules/InferenceModule/main.py
+++ modules/InferenceModule/main.py
@@ -45,7 +45,8 @@ sess = rt.InferenceSession("TinyYOLO.onnx")
 print("loaded after", time.time() - start_time, "s")
 
 IOTHUB_CONNECTION_STRING = os.getenv('MY_IOTHUB_CONNECTION_STRING')
-BLOB_STORAGE_CONNECTION_STRING = "{Blob Storage Connection String}"
+BLOB_STORAGE_CONNECTION_STRING = os.getenv('MY_BLOB_STORAGE_CONNECTION_STRING')
+# BLOB_STORAGE_CONNECTION_STRING = "{Blob Storage Connection String}"
 print("IOTHUB_CONNECTION_STRING", IOTHUB_CONNECTION_STRING)
 
 # Path to CSV FILE (edit if you want)
