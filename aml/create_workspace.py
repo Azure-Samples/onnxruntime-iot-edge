4d0ec8d6728eaa69d45cfa68442badd0a90dedd0
Wolfgang Pauli <wopauli@microsoft.com>
2020-09-17 16:39:42 -0700

    added Readme for aml folder

---
+++ aml/create_workspace.py
@@ -0,0 +1,24 @@
+import json
+from azureml.core import Workspace, Model, VERSION
+from azureml.core.authentication import ServicePrincipalAuthentication
+
+print(VERSION)
+
+with open("aml/config.json", "r") as f:
+    config = json.load(f)
+
+auth = ServicePrincipalAuthentication(
+    config["tenant_id"],
+    config["service_principal_id"],
+    config["service_principal_password"]
+)
+
+ws = Workspace.create(
+    name=config["workspace_name"],
+    auth=auth,
+    subscription_id=config['subscription_id'],
+    resource_group=config['resource_group'],
+    location=config['location']
+    exist_ok=True,
+    show_output=True,
+)
